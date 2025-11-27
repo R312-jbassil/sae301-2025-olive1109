626b1cbc97dca09d7460e05434bdfbf3d6266350
hmabed <hmabed@gmail.com>
2025-11-27 10:16:09 +0100

    Hakim Mabed - changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import netlify from '@astrojs/netlify';
-
+//import netlify from '@astrojs/netlify';
 
 import tailwindcss from '@tailwindcss/vite';
 
